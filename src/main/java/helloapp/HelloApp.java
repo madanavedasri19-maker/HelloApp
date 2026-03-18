86e34b8f10e1c4ba0e053e715715c653b9601d0a
Vedasri Madana <madanavedasri19@gmail.com>
2026-03-18 09:06:17 +0530

    UC1: Display Hello World

--- src/main/java/helloapp/HelloApp.java
+++ src/main/java/helloapp/HelloApp.java
@@ -1,17 +1,8 @@
 package helloapp;
-
-public class HelloApp {
-
-    public static void main(String[] args) {
-
-        // Check if user passed argument
-        if (args.length > 0) {
-            String name = args[0];
-            System.out.println("Hello, " + name + "!");
-        } else {
-            // Default case (same as UC1)
-            System.out.println("Hello, World!");
-        }
-
+public class HelloApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
